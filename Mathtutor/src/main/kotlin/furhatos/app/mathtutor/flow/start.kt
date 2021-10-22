a3ffa6b86bb00c36ad7f735cc87179d28e437868
Laura Ottevanger <laura.ottevanger@hotmail.com>
2021-10-22 17:49:11 +0200

    Update start.kt

--- Mathtutor/src/main/kotlin/furhatos/app/mathtutor/flow/start.kt
+++ Mathtutor/src/main/kotlin/furhatos/app/mathtutor/flow/start.kt
@@ -25,7 +25,7 @@ val Start: State = state(Interaction) {
         val confirm = furhat.askYN("So, your name is " + it.intent.name + ", is that correct?")
         if (confirm == true) {
             furhat.gesture(Gestures.Smile, async = true)
-            furhat.say("Nice to meet you " + it.intent.name "! Let's start the tutoring session!")
+            furhat.say("Nice to meet you " + it.intent.name + "! Let's start the tutoring session!")
             goto(Subject)
         } else {
             furhat.gesture(Gestures.ExpressSad, async = true)
