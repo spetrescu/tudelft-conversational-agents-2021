346d1c2d1cbb04f7673710b9313df4af035d8bcb
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-10-30 09:19:30 +0200

    Refactor code for better Furhat compatibility

--- nlu/inference_server.py
+++ nlu/inference_server.py
@@ -1,3 +1,5 @@
+# ACCESS ALL THE CODEBASE BY DOWNLOADING GOOGLE DRIVE ARCHIVE
+
 from flask import Flask, request, Response, jsonify
 from flask import render_template
 from flask_cors import CORS
@@ -8,6 +10,21 @@ import random
 
 import sys
 sys.path.append('../../')
+
+import logging
+import click
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+def secho(text, file=None, nl=None, err=None, color=None, **styles):
+    pass
+
+def echo(text, file=None, nl=None, err=None, color=None, **styles):
+    pass
+
+click.echo = echo
+click.secho = secho
 
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -38,7 +55,16 @@ rgo_queue = Queue(maxsize=0)
 def parse(sampled_results):
     print('----------------------')
     print(sampled_results)
-    client_send_data_to_furhat(sampled_results)
+    sys_res = sampled_results[0].split('system : ')[-1]
+    if "_" in sys_res and "name" in sampled_results[0]:
+        print(1, sys_res)
+        sys_res = sampled_results[0].split('system : ')[0]
+        print(2, sys_res)
+        sys_res = sys_res.replace(' ','')
+        print(3, sys_res)
+        sys_res = sys_res.split('=')[1]
+        print(4, sys_res)
+    send_model_response_to_furhat(sys_res)
     candidates = []
     for system_response in sampled_results:
         system_response = system_response.split('system :')[-1]
@@ -76,10 +102,10 @@ def compare(key1, key2):
     elif key1.count('[') == key2.count('['):
         return 1 if len(key1.split()) > len(key2.split()) else -1
     else:
-        return -1  
+        return -1
 
 def predictor(context):
-    print("context is ", context)
+    #print("context is ", context)
     context_formated = []
     for idx, i in enumerate(context):
         if idx % 2 == 0:
@@ -87,31 +113,21 @@ def predictor(context):
         else:
             context_formated.append(f'system : {i}')
 
-    print("context_formated", context_formated)
+    #print("context_formated", context_formated, "\n")
+    print(context_formated, "\n")
     sampled_results = sample(context_formated[-1:])
     belief_states, response = parse(sampled_results)
 
     return response, belief_states
 
-def get_usr_input_from_furhat():
-    print("Wait for input from Furhat Client...")
-    port = 5599
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:%s" % port)
-    message = socket.recv_string()
-    print("Furhat Client response: ", message)
-    return message
-       
-def client_send_data_to_furhat(usr_message):
-    print("NOW SENDING DATA TO FURHAT")
+def send_model_response_to_furhat(sys_message):
+    print("Sending model response to Furhat serverNluNlg...")
+    print("Trying to send: ", sys_message)
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5897")
-    socket.send_string(usr_message)
-    message = socket.recv_string()
-    print("NLU Client: Received reply from Furhat Server ", "[", message, "]")
-    print("\n")
+    socket.send_string(sys_message)
+    print("Sent model response to Furhat serverNluNlg...")
 
 global_counter = 0
 @app.route('/generate', methods=['GET','POST'])
@@ -128,14 +144,52 @@ def generate_queue():
     rgo_queue.task_done()
     return jsonify(output)
 
+def get_usr_input_from_furhat():
+    print("\n\nWait for input from Furhat clientNluNlg...")
+    port = 5599
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:%s" % port)
+    message = socket.recv_string()
+    print("Furhat clientNluNlg sent: ", message, "\n")
+    return message
+
 def generate_for_queue(in_queue, out_queue):
-    print("Start NLU system...")
+    print("Start Flask system...")
     context_messages_list = []
-    context_messages_list.append(get_usr_input_from_furhat())
-    
+    idx = 0
+
     while True:
+        print("idx: ", idx)
+        context_messages_list.append(get_usr_input_from_furhat())
         response, belief_states = predictor(context_messages_list)
-        context_messages_list = response
+        context_messages_list.append(response)
+        #print("context_messages_list: \n", context_messages_list + "\n\n")
+        idx += 1
+
+    ## Old workflow from the authors of SOLOIST
+    memory = []
+    while True:
+        _, in_request = in_queue.get()
+        print("in_request['msg'] = ", in_request['msg'])
+        obs = in_request['msg']
+        response, belief_states = predictor(obs)
+        #if belief_states != {}:
+        #    name = belief_states['name']
+        #    memory.append(f'reminder call [name]({name})')
+
+        #followup = ''
+        #if response.strip() == 'action_set_reminder':
+        #    followup = 'Sure thing, added to your reminder list!'
+        #if response.strip() == 'action_forget_reminders':
+        #    followup = 'Sure thing, remove all your reminders!'
+        #    memory = []
+        #res = {}
+        #res['response'] = response
+        #res['memory'] = memory
+        #res['followup'] = followup
+        #out_queue.put(res)
+        #in_queue.task_done()
 
 if __name__ == "__main__":
 
